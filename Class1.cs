73249b8c2a1b7ef5b1e7077e626bca01683446c6
Lawunn Khaing <elakh001@edu.xamk.fi>
2022-10-24 20:05:26 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment10_2_
+{
+    internal class Parking
+    {
+        TimeSpan totalTime;
+        TimeSpan checkinTime;
+        TimeSpan checkoutTime;
+
+        public Parking(TimeSpan checkinTime, TimeSpan checkoutTime)
+        {
+            CheckinTime = checkinTime;
+            CheckoutTime = checkoutTime;
+        }
+        public TimeSpan CheckinTime { get; set; }
+        public TimeSpan CheckoutTime { get; set; }
+
+        public TimeSpan HowLong()
+        {
+            return totalTime = CheckoutTime - CheckinTime;
+
+        }
+
+
+        public double Totalcharges(TimeSpan checkinTime, TimeSpan checkoutTime)
+        {
+            double totalCharge = 0;
+            var start = Convert.ToDouble(checkinTime.TotalHours);
+            var end = Convert.ToDouble(checkoutTime.TotalHours);
+            var difference = end - start;
+            double Time = Math.Ceiling(difference);
+            if (Time <= 2)
+            {
+                totalCharge = Time * 2;
+            }
+            else if (Time > 2 && Time <= 5)
+            {
+                totalCharge = Time * 1.75;
+            }
+            else
+            {
+                totalCharge = Time * 1.5;
+            }
+
+
+            return totalCharge;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Assignment10_2_;
+bool stillMore;
+TimeSpan startLimit = new TimeSpan(7, 0, 0);
+TimeSpan endLimit = new TimeSpan(22, 0, 0);
+TimeSpan start, end;
+double charges;
+do
+{
+
+    Console.Write("Enter the starting time (HH:MM):");
+    string received = Console.ReadLine();
+    while (!TimeSpan.TryParse(received, out start) || start < startLimit || start >= endLimit)
+    {
+        Console.Write("Something went wrong, try again:");
+        received = Console.ReadLine();
+    }
+    Console.Write("Enter the ending time (HH:MM):");
+    received = Console.ReadLine();
+    while (!TimeSpan.TryParse(received, out end) || end < start || end >= endLimit)
+    {
+        Console.Write("Something went wrong, try again:");
+        received = Console.ReadLine();
+    }
+
+
+    Parking myParking = new Parking(start, end);
+    TimeSpan numberofhours = myParking.HowLong();
+
+    var total = myParking.Totalcharges(start, end);
+
+
+    Console.WriteLine("Parking time is {0} hours and {1} minutes.", numberofhours.Hours,
+    numberofhours.Minutes);
+    Console.WriteLine("Total charges is {0} euros", total);
+
+    Console.Write("More of this(Y/N):");
+    string decison = Console.ReadLine().ToUpper();
+    if (decison.StartsWith("Y"))
+        stillMore = true;
+    else
+        stillMore = false;
+}
+while (stillMore);
